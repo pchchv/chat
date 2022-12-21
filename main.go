d53dbd5a2dc25afaba30a756245707485255a633
pchchv <ipchchv@gmail.com>
2022-12-21 12:35:55 +0300

    Implement a manager

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
 	id     string
 	socket *websocket.Conn
-	send   chan []byte
+	send   chan []byte // Message waiting to be sent.
 }
 
+// Keeps track of all connected clients,
+// clients who are trying to register,
+// clients who have been destroyed and are waiting to be deleted,
+// and messages to and from all connected clients.
 type ClientManager struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
@@ -19,4 +27,46 @@ type Message struct {
 	Sender    string `json:"sender,omitempty"`
 	Recipient string `json:"recipient,omitempty"`
 	Content   string `json:"content,omitempty"`
+}
+
+var manager = ClientManager{
+	clients:    make(map[*Client]bool),
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case conn := <-manager.register:
+			manager.clients[conn] = true
+			jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
+			manager.send(jsonMessage, conn)
+		case conn := <-manager.unregister:
+			if _, ok := manager.clients[conn]; ok {
+				close(conn.send)
+				delete(manager.clients, conn)
+				jsonMessage, _ := json.Marshal(&Message{Content: "/A socket has disconnected."})
+				manager.send(jsonMessage, conn)
+			}
+		case message := <-manager.broadcast:
+			for conn := range manager.clients {
+				select {
+				case conn.send <- message:
+				default:
+					close(conn.send)
+					delete(manager.clients, conn)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) send(message []byte, ignore *Client) {
+	for conn := range manager.clients {
+		if conn != ignore {
+			conn.send <- message
+		}
+	}
 }
