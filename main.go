b27724b520b316740331f148d06e9c9d1e78ab30
pchchv <ipchchv@gmail.com>
2022-12-21 12:19:26 +0300

    Implement Client, ClientManager, Message data structs

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
