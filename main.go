7216885afd52c415fadae85e8a366dc2c6bb058c
pchchv <ipchchv@gmail.com>
2022-12-21 12:53:39 +0300

    Implement a wsPage

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Client struct {
@@ -105,4 +108,26 @@ func (c *Client) write() {
 			c.socket.WriteMessage(websocket.TextMessage, message)
 		}
 	}
+}
+
+func wsPage(res http.ResponseWriter, req *http.Request) {
+	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(res, req, nil)
+	if err != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	client := &Client{id: uuid.NewV4().String(), socket: conn, send: make(chan []byte)}
+
+	manager.register <- client
+
+	go client.read()
+	go client.write()
+}
+
+func main() {
+	fmt.Println("Starting application...")
+	go manager.start()
+	http.HandleFunc("/ws", wsPage)
+	http.ListenAndServe(":12345", nil)
 }
